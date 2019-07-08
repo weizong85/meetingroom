bda1a9ba1ef6342d335a6f94eaec1c5b00e0c2f3
Song Weizong <>
2019-07-08 22:16:55 +0800

    add built-in validators

--- server/models/schedule.model.js
+++ server/models/schedule.model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const ScheduleSchema = mongoose.Schema({
-    room_id: String,
-    startTime: String,
-    endTime: String
+    room_id: {
+		type: String,
+		required: true
+	},
+    startTime: {
+		type: Number,
+		required: true
+	},
+    endTime: {
+		type: Number,
+		required: true
+	}
 }, { collection : '_schedule' });
 
 module.exports = mongoose.model('schedule', ScheduleSchema);
